fd48217fc0ff051ee1055b433bfd5c4ab1d28e42
Saatvik Shah <saatvikshah1994@gmail.com>
2021-06-18 23:41:06 +0200

    Extract timestamp from log line
    This patch sets up the function which can parse a given log line
    containing the user supplied timestamp strftime type along with a log
    line. The resulting output can also be compared as it inherits the Ord
    kind.
    Pending additional unit and property based testing of the output.

--- src/LoggyCore.hs
+++ src/LoggyCore.hs
@@ -1,1 +1,13 @@
 module LoggyCore where
+
+import Data.Time
+
+type DateFormat = String
+
+extract_timestamp :: DateFormat -> String -> Maybe UTCTime
+extract_timestamp dformat line = parsed_ts
+    where
+        parsed_ts = case time_parser line of
+            [(ts, _)] -> Just ts
+            _ -> Nothing
+        time_parser = readSTime True defaultTimeLocale dformat
